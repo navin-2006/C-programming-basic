e8be624978721e4681fa44c517f24f94689d9147
navin-2006 <navinroxx4@gmail.com>
2024-10-06 18:45:52 +0530

    Create Even or not .c

---
+++ Even or not .c
@@ -0,0 +1,16 @@
+include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter no :");
+    scanf("%d",&n);
+    if(n%2==0)
+   {
+       printf("given no is even");
+       
+   }
+   else 
+      printf("odd");
+
+    return 0;
+}
