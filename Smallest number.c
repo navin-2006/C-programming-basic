76eba8476cea8b037b46fa67b496da8ee2493152
navin-2006 <navinroxx4@gmail.com>
2024-10-06 19:04:00 +0530

    Create Smallest number.c

---
+++ Smallest number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a<b)
+   {
+       printf("a is smallest");
+       
+   }
+   else 
+      printf("b is smallest");
+
+    return 0;
+}
