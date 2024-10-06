56af794a4865900e74dcd27ade96a9ca56e28fdf
navin-2006 <navinroxx4@gmail.com>
2024-10-06 19:07:23 +0530

    Create Equal or not .c

---
+++ Equal or not .c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a==b)
+   {
+       printf("a is equal to b");
+       
+   }
+   else 
+      printf("a is not equal to b");
+
+    return 0;
+}
