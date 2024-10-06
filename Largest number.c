9606ee8d8cddeec4e4f63cdc8b15fc83ed91dc8c
navin-2006 <navinroxx4@gmail.com>
2024-10-06 19:01:56 +0530

    Create Largest number.c

---
+++ Largest number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a>b)
+   {
+       printf("a is largest");
+       
+   }
+   else 
+      printf("b is largest");
+
+    return 0;
+}
